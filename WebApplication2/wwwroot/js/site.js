dfc236adf3bebca7416af88cfec86a76d12d751e
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-03-31 16:42:35 -0600

    local saved entries and user data

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -1,19 +1,27 @@
 ﻿class Journal {
     constructor() {
+        this.entries = [];
         this.entry = '';
+        this.size=0;
     }
 
     add(_entry) {
-        this.entry = this.entry + " " + _entry;
+        this.entries.push(_entry);
+        this.size++;
     }
 
     display() {
-        alert(this.entry);
-        console.log(this.entry);
+        for(let i=0;i<this.size;i++){
+            alert(this.entries[i]);
+            console.log(this.entries[i]);
+        }
+       
     }
 
-    enterJournal() {// use state here to save journal entry , maybe into a txt file for now locally
+    enterJournal = (ev) => {// use state here to save journal entry , maybe into a txt file for now locally
+        ev.preventDefault();
         const text = document.getElementById("entry");
+        document.forms[0].reset();
         if (text) {
             this.add(text.value);
             this.display();
@@ -23,22 +31,32 @@
 
 class User {
     constructor() {
+       
         this.Name = '';
     }
 
     setName(name) {
-        this.Name = this.Name + name;
+        this.Name = this.Name + " " + name;
     }
 
     display() {
         alert(this.Name);
         console.log(this.Name);
     }
-   getName(){
-        return this.Name;
-   }
-    getData() {
+
+    getName() {
+        if(this.Name){
+            return this.Name;
+        }
+        else{
+            return '0';
+        }
+    }
+
+    getData = (ev) => {
+        ev.preventDefault();
         const text = document.getElementById("name");
+        document.forms[0].reset();
         if (text) {
             this.setName(text.value);
             this.display();
