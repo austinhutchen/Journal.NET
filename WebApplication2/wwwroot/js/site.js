c371c7495c2c14cbd2a726ab1d60825845984a0d
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-03-31 09:01:43 -0600

    user class files

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -1,21 +1,49 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-getData = ( person ) => {
-    const text=document.getElementById("name");
-    if (text){
-        person.Name=text.value;
-        alert("Hello, "+person.Name)
-   // use state to update variables in the actual document without appending
+﻿class Journal {
+    constructor() {
+        this.entry = '';
     }
 
-}
+    add(_entry) {
+        this.entry = this.entry + " " + _entry;
+    }
 
-enterJournal = (journal) => {// use state here to save journal entry , maybe into a txt file for now locally
-    const text=document.getElementById("entry");
-    if (text){
-        journal.entry=text.value;
-       alert(journal.entry)
+    display() {
+        alert(this.entry);
+        console.log(this.entry);
+    }
+
+    enterJournal() {// use state here to save journal entry , maybe into a txt file for now locally
+        const text = document.getElementById("entry");
+        if (text) {
+            this.add(text.value);
+            this.display();
+        }
     }
 }
+
+class User {
+    constructor() {
+        this.Name = '';
+    }
+
+    setName(name) {
+        this.Name = this.Name + name;
+    }
+
+    display() {
+        alert(this.Name);
+        console.log(this.Name);
+    }
+
+    getData() {
+        const text = document.getElementById("name");
+        if (text) {
+            this.setName(text.value);
+            this.display();
+            // use state to update variables in the actual document without appending
+        }
+    }
+}
+
+
+
