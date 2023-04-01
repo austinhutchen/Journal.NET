695e4988cee1a10bace6918eb840be0194a60bc8
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-04-01 16:05:26 -0600

    entries array

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -1,43 +1,37 @@
 ﻿class Journal {
     constructor() {
         this.entries = [];
-        this.entry = '';
-        this.size=0;
+        this.size = 0;
     }
 
-    add(_entry) {
+    add(_entry) {   
         this.entries.push(_entry);
         this.size++;
     }
 
     display() {
-        for(let i=0;i<this.size;i++){
-            alert(this.entries[i]);
-            console.log(this.entries[i]);
+        let string="";
+        for (let i = 0; i < this.size; i++) {
+            string+="ENTRY "+(i+1).toString()+":  "+ this.entries[i]+" ";
         }
-       
+        alert(string);
     }
 
-    enterJournal = (ev) => {// use state here to save journal entry , maybe into a txt file for now locally
-        ev.preventDefault();
-        const text = document.getElementById("entry");
-        document.forms[0].reset();
-        if (text) {
-            this.add(text.value);
-            this.display();
-        }
+    enterJournal = (ev) => { // use state here to save journal entry , maybe into
+                              // a txt file for now locally
+    ev.preventDefault();
+    const text = document.getElementById("entry");
+    if (text) {
+        this.add(text.value);
+        this.display();
     }
+}
 }
 
 class User {
-    constructor() {
-       
-        this.Name = '';
-    }
+    constructor() { this.Name = ''; }
 
-    setName(name) {
-        this.Name = this.Name + " " + name;
-    }
+    setName(name) { this.Name = this.Name + " " + name; }
 
     display() {
         alert(this.Name);
@@ -45,25 +39,21 @@ class User {
     }
 
     getName() {
-        if(this.Name){
+        if (this.Name) {
             return this.Name;
-        }
-        else{
+        } else {
             return '0';
         }
     }
 
     getData = (ev) => {
-        ev.preventDefault();
-        const text = document.getElementById("name");
-        document.forms[0].reset();
-        if (text) {
-            this.setName(text.value);
-            this.display();
-            // use state to update variables in the actual document without appending
-        }
+    ev.preventDefault();
+    const text = document.getElementById("name");
+    document.forms[0].reset();
+    if (text) {
+        this.setName(text.value);
+        this.display();
+        // use state to update variables in the actual document without appending
     }
 }
-
-
-
+}
