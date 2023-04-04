f9cfb09ac236d7fdc8998b4d1cac3adf450fdcf1
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-04-04 10:13:51 -0600

    local storage attempt 1

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -4,34 +4,43 @@
         this.size = 0;
     }
 
-    add(_entry) {   
+    add(_entry) {
         this.entries.push(_entry);
         this.size++;
     }
 
     display() {
-        var string="";
-        for (var i = 0; i < this.size; i++) {
-            string+="ENTRY "+(i+1).toString()+":  "+ this.entries[i]+"\n";
+
+        let string = "";
+        for (let i = 0; i < this.size; i++) {
+            string += "ENTRY " + (i + 1).toString() +  " :  " + this.entries[i] + "\n";
         }
         alert(string);
     }
 
-    enterJournal = (ev) => { // use state here to save journal entry , maybe into
-                              // a txt file for now locally
-    ev.preventDefault();
-    const text = document.getElementById("entry");
-    if (text) {
-        this.add(text.value);
-        this.display();
+    enterJournal = (ev) => {
+        // use state here to save journal entry , maybe into
+        // a txt file for now locally
+        ev.preventDefault();
+        const text = document.getElementById("entry");
+        if (text) {
+            this.add(text.value);
+        
+            this.display();
+        }
+
     }
-}
+
 }
 
 class User {
-    constructor() { this.Name = ''; }
+    constructor() {
+        this.Name = '';
+    }
 
-    setName(name) { this.Name = this.Name + " " + name; }
+    setName(name) {
+        this.Name = this.Name + " " + name;
+    }
 
     display() {
         alert(this.Name);
@@ -47,11 +56,11 @@ class User {
     }
 
     getData = (ev) => {
-    ev.preventDefault();
-    const text = document.getElementById("name");
-    document.forms[0].reset();
+        ev.preventDefault();
+        const text = document.getElementById("name");
+        document.forms[0].reset();
         this.setName(text.value);
         this.display();
         // use state to update variables in the actual document without appending
-}
+    }
 }
